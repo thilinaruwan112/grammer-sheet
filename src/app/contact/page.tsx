6433af1be6efe9aa77b76096daf507741bcda1c4
Thilina Ruwan <payshiasoftwaresolutions@gmail.com>
2025-07-06 11:09:13 +0000

    when i click the conact button to go to conatct page it not load

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,11 +1,9 @@
-import ContactFormSection from '@/components/contact/contact-form-section';
 import HeroSection from '@/components/contact/hero-section';
 
 export default function ContactPage() {
   return (
     <>
       <HeroSection />
-      <ContactFormSection />
     </>
   );
 }
